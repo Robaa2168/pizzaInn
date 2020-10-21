bfc9f8203a61e9eb1005ca2b6a9f5df1f95f02fc
Robaa2168 <71884172+Robaa2168@users.noreply.github.com>
2020-10-21 12:48:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,60 +33,37 @@ function size(x) {
 
 };
 	function checkboxChange(x) {
-		var priceDialog = 700;
+		var priceDialog = 0;
+
+      $("#recipe2"+x).children("li").children("input").each(function () {
+        if($(this).is(":checked")){
+        addedValue = parseInt($(this).attr('value'), 10);
+        priceDialog += addedValue;
+      }
+    });
+
 		$("#recipe"+x).children("li").children("input").each(function () {
 			if($(this).is(":checked")){
-				priceDialog+=50;
+				addedValue = parseInt($(this).attr('value'), 10);
+        priceDialog += addedValue;
 			}
+
 				$("#totalDialog"+x+">span").text(priceDialog);
 			$(this).change(function () {
 				if ($(this).is(":checked")) {
-					priceDialog += 50;
+				addedValue = parseInt($(this).attr('value'), 10);
+        priceDialog += addedValue;
 					console.log(priceDialog);
 					$("#totalDialog"+x+">span").text(priceDialog);
 				}else{
-					priceDialog -= 50;
+					addedValue = parseInt($(this).attr('value'), 10);
+          priceDialog -= addedValue;
 					$("#totalDialog"+x+">span").text(priceDialog);
 				}
 			})
+
 		});
 	}
-$('#size').children('li').click(checkboxChange(2));
-	$('#recipe1').children('li').click(checkboxChange(1));
-
-	$(".addIngredient").on("click", function () {
-
-		var inputIng = '<input type="text" id="newIngredient">';
-		var confirmInput = '<a class="btnStyle3 btnStyle confirmInput">&#10004;</a>';
-		var cancelInput = '<a class="btnStyle3 btnStyle cancelInput">&#10008;</a>';
-		var inputWrap = '<div class="addIngredientWrap">' + inputIng + confirmInput + cancelInput + '</div>'
-		$(this).parent().children("ul").after(inputWrap);
-    $("#newIngredient").focus();
-    $("#newIngredient").attr("placeholder", "separate ingredients with a comma");
-
-		$(".addIngredientWrap > .confirmInput").on("click", function () {
-			if ($("#newIngredient").val() != "") {
-				var newIngredient = ($(".addIngredientWrap input").val()).split(",");
-				var newCheckbox = '<input type="checkbox" checked>';
-
-        for (var i = 0; i < newIngredient.length; i++){
-         $(this).parent().siblings("ul").append("<li>" + newCheckbox + newIngredient[i] + "  (+50Ksh)</li>");
-        }
-
-
-				$('#recipe1').children('li').click(checkboxChange(1));
-				$('#size').children('li').click(checkboxChange(2));
-
-				$(this).parent().remove();
-			}else{
-				$("#newIngredient").attr("placeholder", "Please add Toppings");
-			}
-		});
-		// Remove button hides the input
-		$(".addIngredientWrap > .cancelInput").on("click", function () {
-			$(this).parent().remove();
-		})
-	})// add ingredient button
 
 	$(".listOver").on("click", function () {
 		var orderName = '<h3 class="orderName"><span>' + $(this).parent().siblings(".ui-dialog-titlebar").children("span").text() + '</span><a class="delBtn">&#10008;</a>' +'</h3>';
@@ -100,19 +77,12 @@ $('#size').children('li').click(checkboxChange(2));
 			$(".orderIngredients").last().append("<li>" + selectedIngredient + "</li>");
 		})
 
-		// opens the cart side menu
-		if ($('#cartToggle').prop('checked')) {
-			$("#cartToggle").prop("checked", true);
-		}else{
-			$("#cartToggle").prop("checked", true);
-		}
 
 		$(this).parent(".ui-dialog-content").dialog("close");
 
 		numOfOrders = $("#listOfOrders").children().length;
 		$(".num").text(numOfOrders);
 
-		// display total price in cart orders
 		var totalOrderPrice = 0;
 		$("#listOfOrders").children("li").children(".orderPrice").children("span").each(function () {
 			var price = parseFloat($(this).text());
@@ -120,7 +90,6 @@ $('#size').children('li').click(checkboxChange(2));
 			$(".cart > h3 > span").text(totalOrderPrice + "Ksh");
 		});
 
-		// remove order from cart
 		$(".delBtn").on("click", function () {
 			var removePrice = $(this).parent().parent().children(".orderPrice").children("span").text();
 			totalOrderPrice -= removePrice;
@@ -130,7 +99,7 @@ $('#size').children('li').click(checkboxChange(2));
 			numOfOrders = $("#listOfOrders").children().length;
 			$(".num").text(numOfOrders);
 		})
-	}); // List over (done button)
+	}); 
 
 	$(".finishOrder").on("click", function () {
      $("#finalOrderList > ol").children().remove();
@@ -169,6 +138,4 @@ $('#size').children('li').click(checkboxChange(2));
 			$("#buyerInfo").append('<p>Fill up all the inputs</p>');
 		}
 	})
-
-})
-};
+});
